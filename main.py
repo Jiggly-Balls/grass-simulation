57f4749d8ebd39ec13e9d47c6805e038f6d2d918
Jiggly Balls <jigglyballs9000@gmail.com>
2026-01-29 21:05:01 +0530

    Added process_loader

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 import esper
 import pygame
 from game_state import StateManager
 
 from data.constants import FPS, SCREEN_RESOLUTION, WorldEnum
+from ecs.processes import MovementProcess
 from states.base_state import BaseState
+
+if TYPE_CHECKING:
+    from esper import Processor
 
 __version__ = "0.1.0"
 
@@ -12,12 +20,18 @@ pygame.display.init()
 pygame.display.set_caption("Gass Simulation v" + __version__)
 
 
+def load_processes() -> dict[str, Processor]:
+    return {"process_movement": MovementProcess()}
+
+
 def main() -> None:
     window = pygame.display.set_mode(SCREEN_RESOLUTION)
     clock = pygame.time.Clock()
 
     manager = StateManager[BaseState](
-        window=window, bound_state_type=BaseState
+        bound_state_type=BaseState,
+        window=window,
+        **load_processes(),
     )
     manager.connect_state_hook("states.main_state", clock=clock)
     manager.change_state(state_name=WorldEnum.MAIN)
