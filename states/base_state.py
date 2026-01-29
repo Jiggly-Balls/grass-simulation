df725585bf41f0a64f130d2eb2697f46a63e4618
Jiggly Balls <jigglyballs9000@gmail.com>
2026-01-29 21:06:12 +0530

    Added process_movement attribute

--- states/base_state.py
+++ states/base_state.py
@@ -4,14 +4,18 @@ from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING
 
 from game_state import State
+from game_state.utils import MISSING
 
 if TYPE_CHECKING:
     from pygame import Surface
     from pygame.event import Event
 
+    from ecs.processes import MovementProcess
+
 
 class BaseState(State["BaseState"], ABC):
-    window: Surface
+    window: Surface = MISSING
+    process_movement: MovementProcess = MISSING
 
     @abstractmethod
     def process_event(self, event: Event) -> None: ...
