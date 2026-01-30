67cbd15bc896bd31ee99868e3145532d72059c72
Jiggly Balls <jigglyballs9000@gmail.com>
2026-01-30 21:57:26 +0530

    Added process_add_grass & process_render attrs to BaseState

--- states/base_state.py
+++ states/base_state.py
@@ -10,12 +10,14 @@ if TYPE_CHECKING:
     from pygame import Surface
     from pygame.event import Event
 
-    from ecs.processes import MovementProcess
+    from ecs.processes import AddGrassProcess, MovementProcess, RenderProcess
 
 
 class BaseState(State["BaseState"], ABC):
     window: Surface = MISSING
     process_movement: MovementProcess = MISSING
+    process_add_grass: AddGrassProcess = MISSING
+    process_render: RenderProcess = MISSING
 
     @abstractmethod
     def process_event(self, event: Event) -> None: ...
