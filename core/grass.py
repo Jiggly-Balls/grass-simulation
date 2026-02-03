78e54fcfd27aabaab6caa0cf4aa4b4e10c29f35e
Jiggly Balls <jigglyballs9000@gmail.com>
2026-02-04 00:32:41 +0530

    Reinitialized GrassManager & GrassSprite

--- core/grass.py
+++ core/grass.py
@@ -2,85 +2,28 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-import pygame
-
 if TYPE_CHECKING:
-    from pygame import Rect, Surface, Vector2
+    from collections.abc import Sequence
+
+    from pygame import Surface, Vector2
 
 
-class GrassSprite:
+class GrassSprite[G: str | int]:
     def __init__(
         self,
-        image: Surface,
+        image_id: G,
         position: Vector2,
     ) -> None:
-        self.image: Surface = image
-        self.rect: Rect = image.get_rect()
-        self.rect.center = position
+        self.image_id: G = image_id
+        self.position: Vector2 = position
 
 
-class GrassGroup:
-    def __init__(self) -> None:
-        screen_size = pygame.display.get_surface()
-        assert screen_size is not None
+class GrassManager[G: str | int]:
+    def __init__(self, grass_objects: dict[G, Surface]) -> None:
+        self.grass_objects: dict[G, Surface] = grass_objects
 
-        self.surf_width: int
-        self.surf_height: int
-        self.surf_width, self.surf_height = screen_size.get_size()
-        self.grass_surf: Surface = pygame.Surface(
-            (self.surf_width, self.surf_height),
-            # pygame.SRCALPHA,
-        )
-        self.grass_objects: list[GrassSprite] = []
+    def add(
+        self, position: Vector2, grass_variants: None | Sequence[G] = None
+    ) -> None: ...
 
-    def add(self, grass_sprite: GrassSprite) -> None:
-        self.grass_objects.append(grass_sprite)
-        self.grass_objects.sort(key=lambda sprite: sprite.rect.y)
-
-        # SELECTIVE REDRAWING-
-
-        rect_size = self.grass_surf.get_rect()
-        rect_size.topleft = grass_sprite.rect.topleft
-        rect_size.width = 10
-        pygame.draw.rect(
-            self.grass_surf,
-            (0, 0, 0),
-            (*grass_sprite.rect.center, 10, 10),
-        )
-
-        for_redraw: list[tuple[Surface, Rect]] = [
-            (sprite.image, sprite.rect)
-            for sprite in self.grass_objects
-            if grass_sprite.rect.x + 15
-            > sprite.rect.x
-            > grass_sprite.rect.x - 15
-            # and
-            # grass_sprite.rect.y + 10 > sprite.rect.y > grass_sprite.rect.y - 10
-        ]
-        for_redraw.sort(key=lambda source: source[1].y)
-
-        # self.grass_surf.blits(
-        #     (sprite.image, sprite.rect.topleft)
-        #     for sprite in self.grass_objects
-        #     if grass_sprite.rect.x + 30 > sprite.rect.x > grass_sprite.rect.x - 30
-        #     and
-        #     grass_sprite.rect.y + 30 > sprite.rect.y > grass_sprite.rect.y - 30
-        # )
-        self.grass_surf.blits(
-            (sprite[0], sprite[1].topleft) for sprite in for_redraw
-        )
-
-        # NORMAL RENDERING-
-
-        # self.grass_surf.blits(
-        #     (sprite.image, sprite.rect.topleft)
-        #     for sprite in self.grass_objects
-        # )
-
-    def draw(self, surface: Surface, offset: Vector2) -> None:
-        # surface.blits(
-        #     (spr.image, spr.rect.topleft + offset) for spr in self.sprites()
-        # )
-        surface.blit(
-            self.grass_surf, self.grass_surf.get_rect().topleft + offset
-        )
+    def draw(self, surface: Surface, offset: Vector2) -> None: ...
