e9ed8ef0f995d87f5d2c0bb786f2f7836619c1a5
Jiggly Balls <jigglyballs9000@gmail.com>
2026-02-03 19:13:34 +0530

    Remove SPRITE_TILE_SIZE constant

--- data/constants.py
+++ data/constants.py
@@ -4,7 +4,6 @@ from enum import StrEnum, auto
 
 __all__ = (
     "FPS",
-    "SPRITE_TILE_SIZE",
     "SCREEN_RESOLUTION",
     "BASE_ASSET_PATH",
     "GRASS_SPRITE_SHEET",
@@ -12,7 +11,6 @@ __all__ = (
 )
 
 FPS: int = 60
-SPRITE_TILE_SIZE: int = 32
 GRASS_ABUNDANCE: int = 200
 SCREEN_RESOLUTION: tuple[int, int] = (1000, 600)
 SPEED: int = 500
